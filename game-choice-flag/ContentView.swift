8a3c8dbdc4f5f7c8d364c8c82d56737721ff2127
max-getlife <max.getlife@yahoo.com>
2022-06-04 19:14:46 +0300

    Initial Commit

---
+++ game-choice-flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  game-choice-flag
+//
+//  Created by Maksym Getlife on 04.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ game-choice-flag/game_choice_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  game_choice_flagApp.swift
+//  game-choice-flag
+//
+//  Created by Maksym Getlife on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct game_choice_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
